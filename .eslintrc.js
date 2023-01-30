2321d08835a1b4c078e39e4eefaf27542f1e4b9f
João Victor Geronimo de Souza <Joao.Souza@dinamio.com.br>
2023-01-30 16:10:49 -0300

    first

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-multi-spaces': ['error'],
+        'vue/no-multiple-template-root': 'off',
+        'no-use-before-define': 'off',
+        'no-unused-vars': 'off'
+    }
+}
---
+++ src/views/ProductView.vue
@@ -0,0 +1,47 @@
+<template>
+    <div v-for="prod in products" :key="prod.id">{{ prod.name }}</div>
+</template>
+
+<script>
+export default {
+    name: 'ProductView',
+    data() {
+        return {
+            products: [
+                {
+                    id: 1,
+                    name: 'Product 1',
+                    price: 100,
+                    description: 'Description Product 1'
+                },
+                {
+                    id: 2,
+                    name: 'Product 2',
+                    price: 200,
+                    description: 'Description Product 2'
+                },
+                {
+                    id: 3,
+                    name: 'Product 3',
+                    price: 300,
+                    description: 'Description Product 3'
+                },
+                {
+                    id: 4,
+                    name: 'Product 4',
+                    price: 400,
+                    description: 'Description Product 4'
+                },
+                {
+                    id: 5,
+                    name: 'Product 5',
+                    price: 500,
+                    description: 'Description Product 5'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style></style>
